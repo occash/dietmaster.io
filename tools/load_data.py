8758a6b77d36d736606b1a9fce09917859d3c601
occash <artiom.shal@gmail.com>
2015-11-21 17:23:47 +0300

    Improve load tool

--- tools/load_data.py
+++ tools/load_data.py
@@ -1,31 +1,50 @@
-﻿import csv
+﻿import re
+import csv
 import pymongo
-
-print(pymongo.version)
 
 def to_number(s):
     s = s.strip()
     s = s.replace(',', '.')
     return float(s) if s else 0.0
 
-with open('data.csv', 'r') as file:
-    reader = csv.reader(file, delimiter=';', quotechar='"')
-    next(reader)
+with open('abbrev.csv', 'r') as abbrev_file:
+    abbrev_reader = csv.reader(abbrev_file, delimiter=';', quotechar='"')
 
-    client = pymongo.MongoClient()
-    database = client.dietmaster
-    food = database.food
+    abbrevs = {}
+    for row in abbrev_reader:
+        abbrevs[row[1]] = row[0]
 
-    for row in reader:
-        record = {
-            'name': row[1].lower(),
-            'group': row[2].lower(),
-            'water': to_number(row[4]),
-            'energy': to_number(row[5]),
-            'protein': to_number(row[6]),
-            'lipid': to_number(row[7]),
-            'carbohydrate': to_number(row[8]),
-            'gi': to_number(row[3]),
-        }
+    with open('data.csv', 'r') as data_file:
+        data_reader = csv.reader(data_file, delimiter=';', quotechar='"')
+        next(data_reader)
 
-        food.insert(record)
+        client = pymongo.MongoClient()
+        database = client.dietmaster
+        food = database.food
+
+        regex = r'\b(%s)\b' % '|'.join(abbrevs.keys())
+        pattern = re.compile(regex)
+        # TODO: replace W/ and WO/
+        for row in data_reader:
+            full_name = pattern.sub(lambda x: abbrevs[x.group()], row[1])
+            full_name = full_name.title()
+            attributes = full_name.split(',')
+            short_name = attributes[0]
+
+            record = {
+                'fullname': full_name,
+                'name': short_name,
+                'attributes': attributes,
+                'group': row[3].title(),
+                'water': to_number(row[5]),
+                'energy': to_number(row[6]),
+                'protein': to_number(row[7]),
+                'lipid': to_number(row[8]),
+                'carbohydrate': to_number(row[10]),
+                'gi': to_number(row[4]),
+            }
+
+            try:
+                food.insert(record)
+            except:
+                pass
